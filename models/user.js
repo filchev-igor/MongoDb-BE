ae317325e1d3b54de89ed42fe97025d71b807da5
Igor Filchev <filchev.igor@gmail.com>
2024-12-13 02:43:08 +0200

    Update in mongodb

--- models/user.js
+++ models/user.js
@@ -1,14 +1,11 @@
-//import mongoose from "mongoose";
-
 const mongoose = require("mongoose");
 
-// Define the schema for the 'users' collection
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   age: { type: Number, required: true },
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
 });
 
-// Create and export the model
 const User = mongoose.model("User", userSchema);
+
 module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -2,30 +2,35 @@ var express = require("express");
 var router = express.Router();
 const User = require("../models/user");
 
-/* GET users listing. */
 router.get("/", async function (req, res, next) {
-  //res.send("respond with a resource");
-
   try {
-    const users = await User.find(); // Fetch all users
+    const users = await User.find();
 
-    //const users = await User.find({});
+    const normalizedUsers = users.map((user) => {
+      const { _id, ...rest } = user.toObject();
 
-    res.json(users);
+      return { id: _id, ...rest };
+    });
+    res.json(normalizedUsers);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
+});
 
-  /*
-  User.find()
-    .then(function (response) {
-      res.json(response);
-    })
-    .catch(function (err) {
-      res.json(err);
-    });
+router.post("/", async function (req, res, next) {
+  try {
+    const { name, age, username } = req.body;
 
-     */
+    const newUser = new User({ name, age, username });
+
+    const savedUser = await newUser.save();
+
+    const { _id, ...rest } = savedUser.toObject();
+
+    res.status(201).json({ id: _id, ...rest });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 module.exports = router;
